f77a55ced2df7f82881fc7a8c30cc2fdec8a3532
Hideto Yasunori <hideto0118yasunori@gmail.com>
2017-04-23 12:34:01 -0700

    first commit

---
+++ simpleSNS/FeedCell.swift
@@ -0,0 +1,25 @@
+//
+//  FeedCell.swift
+//  simpleSNS
+//
+//  Created by 康乗秀人 on 2017/04/23.
+//  Copyright © 2017 康乗秀人. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var imgPhoto: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- simpleSNS/ViewController.swift
+++ simpleSNS/ViewController.swift
@@ -8,18 +8,110 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var arrPhotos: [String] = [
+        "timeline01.jpg",
+        "timeline02.jpg",
+        "timeline03.jpg",
+        "timeline04.jpg"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Table view
+        tableView.rowHeight = 415 as CGFloat
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        // (* Required) Number of rows
+        return arrPhotos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        // (*Required) Cell appearance
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell", for: indexPath) as! FeedCell
+        cell.imgPhoto.image = UIImage(named: arrPhotos[indexPath.row])
+        cell.imgPhoto.contentMode = .scaleAspectFill
+        cell.imgPhoto.clipsToBounds = true
+        
+        return cell
+    }
+    
+    // Mark: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        //(* Optional) Header size zero
+        return CGFloat.leastNormalMagnitude
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        
+        //(* Optional) Header size zero
+        return CGFloat.leastNormalMagnitude
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
