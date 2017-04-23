cd3a4035b951d59a76f03c1777c3c0a3b52a0a39
Hideto Yasunori <hideto0118yasunori@gmail.com>
2017-04-23 10:21:18 -0700

    Initial Commit

---
+++ simpleSNS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleSNS
+//
+//  Created by 康乗秀人 on 2017/04/23.
+//  Copyright © 2017 康乗秀人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
